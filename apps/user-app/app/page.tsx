c8080bc048d08130becc58f048e0ebd6cff7238f
Pankaj Singh <165186164+pnk-ltoc-892@users.noreply.github.com>
2024-10-04 23:32:40 +0530

    Initialize next-auth & Still Getting A Error While Doing Auth

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,16 +1,26 @@
 "use client"
-import Image from "next/image";
+
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
 
 import { useBalance } from "@repo/store/useBalance"
+import { Appbar } from "@repo/ui/appbar";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
   const balance = useBalance()
+
+  const session = useSession()
+
+
   return (
     <div className={styles.page}>
       <h1 className="bg-slate-950 text-white h-10 w-96" >Balance: {balance}</h1>
       <h1 className="bg-slate-950 text-white h-10 w-96" >Checking If tailwindCSS got configured Right</h1>
+      <h1 className="bg-slate-950 text-white h-10 w-96" >Checking Auth-Thingy</h1>
+
+      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
+      
     </div>
   );
 }
